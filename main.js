41c7897221d2d5a423425f6c29ccb08ff89a0f99
João Pedro Soares <soaresjoaopedro.ti@gmail.com>
2022-10-14 20:32:47 -0500

    modulo 4

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-function pom(){
-    document.querySelector('#som_tecla_pom').play()
+function sons(idElementoAudio){
+    document.querySelector(idElementoAudio).play()
 }
-document.querySelector('.tecla_pom').onclick = pom
+
+const listaDeTeclas = document.querySelectorAll('.tecla')
+
+
+let contador = 0
+while(contador<listaDeTeclas.length){
+
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1]
+
+    
+    const idAudio = `#som_${instrumento}`
+    console.log(idAudio)
+    tecla.onclick = function(){
+        sons(idAudio)
+    }
+    contador = contador + 1 
+    console.log(contador)
+}
