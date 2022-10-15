a5f5cac0e99b69dfa5fc39bda13ad268a32f1bc4
João Pedro Soares <soaresjoaopedro.ti@gmail.com>
2022-10-14 20:37:38 -0500

    aplicando for

--- main.js
+++ main.js
@@ -5,18 +5,14 @@ function sons(idElementoAudio){
 const listaDeTeclas = document.querySelectorAll('.tecla')
 
 
-let contador = 0
-while(contador<listaDeTeclas.length){
+
+for(let contador = 0; contador<listaDeTeclas.length; contador++){
 
     const tecla = listaDeTeclas[contador]
     const instrumento = tecla.classList[1]
-
-    
     const idAudio = `#som_${instrumento}`
-    console.log(idAudio)
+    
     tecla.onclick = function(){
         sons(idAudio)
     }
-    contador = contador + 1 
-    console.log(contador)
 }
